219e21c3f14d2e11538be11957e1cd8ec3e31280
TessLMAO <110305665+TessLMAO@users.noreply.github.com>
2022-08-02 14:53:49 -0300

    Add files via upload

---
+++ assets/preload/data/gaster/font.lua
@@ -0,0 +1,11 @@
+function onCreatePost()
+
+	setTextFont('timeTxt', 'wingdings-pixelated.ttf')
+	setTextSize('timeTxt', 24)
+	setTextFont('botplayTxt', 'wingdings-pixelated.ttf')
+	setTextSize('botplayTxt', 24)
+	setProperty('botplayTxt.x', getProperty('botplayTxt.x') - 8)
+	setTextFont('scoreTxt', 'wingdings-pixelated.ttf')
+	setTextSize('scoreTxt', 24)
+
+end
